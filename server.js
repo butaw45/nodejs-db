d42174061e076c4dd469c1dc63408782716cfbe1
haris <kumantheas@gmail.com>
2022-06-04 12:20:36 +0700

    insert sql

--- server.js
+++ server.js
@@ -1,42 +1,48 @@
 const express = require('express');
 // const { json } = require('express/lib/response');
-const mysql = require('mysql')
+const mysql = require('mysql');
+const BodyParser = require('body-parser');
 
 const app = express();
 
+app.use(BodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
-app.set("views", "views");
+app.set('views', 'views');
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "school"
-})
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'school',
+});
 
 db.connect((err) => {
-    if (err) {
-        throw err
-    }
-    console.log('MySql Connected...')
+  if (err) {
+    throw err;
+  }
+  console.log('MySql Connected...');
 
-    const sql = "SELECT * FROM user"
+  // Untuk get data
+  app.get('/', (req, res) => {
+    const sql = 'SELECT * FROM user';
     db.query(sql, (err, result) => {
-        // if (err) throw err
-        const users = JSON.parse(JSON.stringify(result))
+      const users = JSON.parse(JSON.stringify(result));
+      // console.log("hasil database ->",users)
+      res.render('index', { users: users, title: 'Data Murid Kelas' });
+    });
+  });
 
-        console.log("hasil database ->",users)
-
-        app.get('/', (req, res) => {
-            res.render("index", {users: users, title: "Data Murid Kelas"})
-        })
-
-    })
-
-    
-
-})
+  // Untuk post data
+  app.post('/tambah', (req, res) => {
+    const insert = `INSERT INTO user (nama, kelas) VALUES ('${req.body.nama}', '${req.body.kelas}');`;
+    db.query(insert, (err, result) => {
+      if (err) throw err;
+      res.redirect('/');
+    });
+  });
+});
 
 app.listen(8000, () => {
-    console.log('Server started on port 8000');
-    });
+  console.log('Server started on port 8000');
+});
